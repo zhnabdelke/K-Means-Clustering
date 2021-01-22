25517d1d11ad96e17f29e61c85879a8abc435df2
zhnabdelke <zhnabdelke@gmail.com>
2021-01-22 18:28:22 -0500

    add component retrieval

---
+++ pca.py
@@ -0,0 +1,39 @@
+import numpy as np
+from numpy.linalg import eig
+
+
+class PCA:
+    def __init(self, targetDimensions):
+
+        self.targetDimensions = targetDimensions
+        self.pc = None
+        self.mean = None
+
+    def retrieve_components(self, data):
+
+        self.mean = np.mean(data, axis=0)
+
+        data -= self.mean
+
+        # transpose the data and create the covariance matrix
+        # where the covariance is with respect to the N features
+        covariance = np.cov(data.T)
+
+        eigenvalues = eig(covariance)[0]
+        eigenvectors = eig(covariance)[1]
+
+        # transpose the eigenvectors (column vectors)
+        # to facilitate calculations
+        eigenvectors = eigenvectors.T
+
+        # sort the eigenvalues in descending order based on value
+        # and store the sorted indices
+        eigenvaluesIndices = np.argsort(eigenvalues)[::-1]
+
+        # store the sorted eigenvalues and eigenvectors
+        eigenvalues = eigenvalues[eigenvaluesIndices]
+        eigenvectors = eigenvectors[eigenvaluesIndices]
+
+        # the components are the eigenvectors from indices 0 to N
+        # target dimensions
+        self.pc = eigenvectors[: self.targetDimensions]
